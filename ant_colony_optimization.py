a00d5078e0b600ba576f3f59c078c7bbe8cce7e2
zwir001 <259128@student.pwr.edu.pl>
2022-12-31 17:04:21 +0100

    added comments to code

--- ant_colony_optimization.py
+++ ant_colony_optimization.py
@@ -8,6 +8,7 @@ import numpy as np
 # ========================================= Graph data =========================================
 
 class Graph:
+    # stwórz instancję grafu
     def __init__(self, matrix: np.ndarray, alpha: float, beta: float, rho: float, q: float):
         self.matrix = matrix
         self.alpha = alpha
@@ -18,10 +19,14 @@ class Graph:
         self.optimal_value = sys.maxsize
         self.pheromone_intensity = np.full((self.n, self.n), self.n / nearest_neighbour(self.matrix))
 
+    # odśwież intensywność feromonów na wszystkich wierzchołkach
     def update_pheromones(self, new_pheromones: list[tuple]):
+        # odparuj feromony
         self._evaporate_pheromones()
+
+        # zwiększ ilość feromonów na krawędzach przebytych przez mrówki według wzoru algorytmu QAS (Wzór 4)
         for pair in new_pheromones:
-            self.pheromone_intensity[pair[0]][pair[1]] += self.q * self.matrix[pair[0]][pair[1]]
+            self.pheromone_intensity[pair[0]][pair[1]] += self.q / self.matrix[pair[0]][pair[1]]
 
     def _evaporate_pheromones(self):
         self.pheromone_intensity = np.multiply(self.pheromone_intensity, self.rho)
@@ -59,6 +64,7 @@ class Graph:
 # ===================================== Ant population data ====================================
 
 class AntColony:
+    # stwórz generację mrówek
     def __init__(self, n: int):
         self.n = n
         self.paths = np.empty(n, dtype=list)
@@ -71,21 +77,28 @@ class AntColony:
                 self.paths[i] = list([i])
             self.not_visited[i] = list([j for j in range(n) if j != i])
 
+    # przemieść mrówki
     def move_all(self, graph: Graph) -> list[tuple]:
         moves = list()
+        # rób dopóki licznik 'ant+no' jest mniejszy od liczby mrówek
         for ant_no in range(self.n):
+            # wybierz losowo kolejny wierzchołek dla mrówki z prawdopodobieństwem opisanym wzorem (Wzór 2)
             next_node = graph.pick_next_node(self.paths[ant_no][-1], self.not_visited[ant_no])[0]
 
+            # dodaj ruch z wierzchołka aktualnego do nowego wylosowanego do listy ruchów
             moves.append(tuple([self.paths[ant_no][-1], next_node]))
 
-            self.paths[ant_no].append(next_node)
-            self.not_visited[ant_no].remove(next_node)
+            self.paths[ant_no].append(next_node)  # dodaj wylosowany wierzchołek do ścieżki mrówki
+            self.not_visited[ant_no].remove(next_node)  # usuń wylosowany wierzchołek z listy nieodwiedzonych
 
+        # zwróć listę ruchów mrówek
         return moves
 
+    # sprawdź ścieżki pokonane przez mrówki
     def check_population_paths(self, graph):
         unipath_counter = 0
-        for path in self.paths:
+
+        for path in self.paths:  # rób dla wszystkich ścieżek mrówek
             path.append(path[0])
             if graph.revalidate_optimal(path):
                 unipath_counter += 1
@@ -95,36 +108,44 @@ class AntColony:
 
 def aco(array: np.ndarray, alpha: float, beta: float, rho: float, q: float, ants_to_stop: int) -> int:
     n = array.shape[0]
-    cycles = numpy.ceil(ants_to_stop / n)
-    graph = Graph(array, alpha, beta, rho, q)
-    for cycle_no in range(int(cycles)):
-        ant_generation = AntColony(n)
-        for i in range(n - 1):
-            new_pheromones = ant_generation.move_all(graph)
-            graph.update_pheromones(new_pheromones)
+    cycles = numpy.ceil(ants_to_stop / n)  # oblicz liczbę generacji mrówek
 
-        if ant_generation.check_population_paths(graph):
+    # stwórz instancję grafu zawierającą tablicę sąsiedztwa, tablicę intensywności feromonów
+    graph = Graph(array, alpha, beta, rho, q)
+
+    for cycle_no in range(int(cycles)):  # rób dopóki licznik 'cycle_no' mniejszy od liczby generacji mrówek
+        ant_generation = AntColony(n)  # stwórz generację mrówek
+        for i in range(n - 1):  # rób dopóki licznik 'i' jest mniejszy od wielkości instancji pomniejszonej o 1
+            new_pheromones = ant_generation.move_all(graph)  # przemieść mrówki do kolejnych nieodwiedzonych wierzchołków
+            graph.update_pheromones(new_pheromones)  # odśwież intensywność feromonów na wszystkich wierzchołkach
+
+        # sprawdź ścieżki pokonane przez mrówki
+        if ant_generation.check_population_paths(graph):  # jeżeli mrówki miały równe wartości ścieżek
             break
 
-    return graph.optimal_value
+    return graph.optimal_value  # zwróć optymalną wartość ścieżki
 
 
 def nearest_neighbour(array: np.ndarray) -> int:
+    # dodaj wierzchołek początkowy (0) do listy odwiedzonych, oraz jako aktualny wierzchołek
     n, visited, path_value, current_node = array.shape[0], list([0]), 0, 0
 
+    # rób dopóki długość listy odwiedzonych jest mniejsza od wielkości instancji
     while len(visited) < n:
+        # znajdź najbliższego sąsiada aktualnego wierzchołka, należącego do wierzchołków nieodwiedzonych
         lowest, nearest_node = sys.maxsize, None
 
         for i in [i for i in range(n) if i not in visited]:
             if array[current_node][i] < lowest:
                 lowest, nearest_node = array[current_node][i], i
 
-        path_value += lowest
-        visited.append(nearest_node)
+        path_value += lowest  # dodaj krawędź między aktualnym a wyznaczonym wierzchołkiem do wartości ścieżki
+        visited.append(nearest_node)  # dodaj wyznaczony wierzchołek do listy odwiedzonych
 
-        current_node = nearest_node
+        current_node = nearest_node  # ustaw wyznaczony wierzchołek jako nowy aktualny
 
-    visited.append(0)
+    visited.append(0)  # dodaj powrót z ostatniego wierzchołka do początkowego do ścieżki
+    # dodaj wartość krawędzi z ostatniego wierzchołka do początkowego do wartości ścieżki
     path_value += array[current_node][0]
 
-    return path_value
+    return path_value  # zwróć wartość ścieżki
--- main.py
+++ main.py
@@ -11,30 +11,38 @@ import helpers as h
 
 if __name__ == '__main__':
     config = h.read_config()  # odczytaj plik ini
-    config.pop(0)
+    config.pop(0)  # usuń nagłówek z opisem parametrów w pierwszej linii
 
     results_csv = list()
 
+    # wczytaj parametry alpha, beta, rho z pliku konfiguracyjnego
     config_data_line = config.pop(0)
     results_csv.append(config_data_line)
     alpha, beta, rho = float(config_data_line[0]), float(config_data_line[1]), float(config_data_line[2])
 
+    # rób dopóki plik zawiera kolejne linie
     for line in config:
-        if len(line) > 1:
-            results_csv.append(line)
+        if len(line) > 1:  # jeżeli ilość wartości oddzielonych przecinkami w linii, większa od 1 to
+            results_csv.append(line)  # zapisz linię do pliku wynikowego
 
-            print(line[0])
+            print(line[0])  # wypisz nazwę instancji
 
-            if int(line[1]) != 0:
+            if int(line[1]) != 0:  # jeżeli liczba wykonań jest różna od 0
+                # wczytaj dane o instancji
                 distances_matrix = h.read_instance_format_tsp(line[0])
                 instance_size = distances_matrix.shape[0]
 
+                # rób dopóki licznik 'repeat' jest mniejszy od liczby wykonań wykonań
                 for repeat in range(int(line[1])):
-                    print(repeat)
+                    print(repeat + 1)  # wypisz numer powtórzenia
                     start = perf_counter()  # rozpocznij pomiar czasu
-                    result = aco(distances_matrix, alpha, beta, rho, 10, 1000)
+                    result = aco(distances_matrix, alpha, beta, rho, 100, 1000)  # wywołaj algorytm
                     end = perf_counter()  # zakończ pomiar czasu
+
+                    # dokonaj pomiaru pamięci
                     mem_usage = memory_usage((aco, (distances_matrix, alpha, beta, rho, 100, 5)))
+
+                    # zapisz zmierzony czas, maksymalne zużycie pamięci, oraz wynik algorytmu do pliku wynikowego
                     results_csv.append(
                         [
                             format_scientific(end - start, locale="pl"),
@@ -43,4 +51,4 @@ if __name__ == '__main__':
                         ]
                     )
         else:
-            h.write_to_csv(line[0], results_csv)
+            h.write_to_csv(line[0], results_csv)  # zapisz plik wynikowy .csv
